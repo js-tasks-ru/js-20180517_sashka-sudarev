c218a72160b454bdcc1c9b30d7191e51eb39971b
Sudarev Aleksandr <gubernator1900@mail.ru>
2018-06-14 00:44:59 +0300

    Решены задачи модулей 1-5

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,8 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(data, age) {
+
+function showSalary(arr,maxAge){
+    let newArr = arr.map( obj => (obj.age<=maxAge) ? `${obj.name}, ${obj.balance}` : false);
+    return (newArr.filter(n => n)).join('\n');;
 }
-
-
--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,13 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let tr=table.querySelectorAll('tbody tr');
+    for (let k in tr){
+        if (k=='length') break;
+        let td=tr[k].querySelectorAll('td');
+        if (td[3].dataset.available===undefined) tr[k].hidden=true;
+        else tr[k].classList.add((td[3].dataset.available=='true') ? 'available' : 'unavailable');
+        tr[k].classList.add((td[2].innerHTML=='m') ? 'male' : 'female');
+        if (+td[1].innerHTML<18) tr[k].style.textDecoration='line-through';
+    }
 }
