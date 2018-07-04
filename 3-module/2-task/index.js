bc8a3749ff96eb10c6b65a91405bec2589bb27e2
Sudarev Aleksandr <gubernator1900@mail.ru>
2018-07-04 13:54:27 +0300

    решены задачи 3.2 6.1 7.1

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,34 @@
 let calendar = {
-    from: new Date('2018-01-01T15:09:10Z'),
-    to: new Date('2018-02-01T10:09:10Z')
+    from: new Date('2018-01-02T15:09:10Z'),
+    to: new Date('2018-02-02T10:09:10Z')
 };
 
-calendar[Symbol.iterator] = function () {
-};
+
+calendar[Symbol.iterator]=function() {
+    let current = +this.from;
+    let last = +this.to;
+
+    return {
+        next() {
+            if (current <= last) {
+                let strDate = (new Date(current));
+                let day=strDate.getDay();
+                let date=strDate.toISOString().slice(8,10);
+                let str = (day==6 || day==0) ? `[${date}]` : ''+date;
+                current+=1000*60*60*24;
+                return {
+                    done: false,
+                    value: str
+                };
+            } else {
+                return {
+                    done: true
+                };
+            }
+        }
+    }
+}
+
+
 
 
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -4,7 +4,40 @@
  * Функция возвращает строкой, сколько времени осталоьс до события
  * @param {Date} when - дата события
  * @return {string} - строка с указанием времени до начала события
- */
+ *//*
 function getBeforeTime(when) {
     let current = Date.now(); // обязательно получать текущую дату через эту функцию. Иначе тесты работать не будут
 }
+*/
+
+function getBeforeTime(when) {
+    let current = Date.now();
+    if (current>=when) return 'событие завершилось';
+    let cur=new Date(current);
+    let res=[];
+    let mainArr=[
+        {set:'setFullYear', get:'getFullYear', n:' г.'},
+        {set:'setMonth', get:'getMonth', n:' мес.'},
+        {set:'setDate', get:'getDate', n:' д.'},
+        {set:'setHours', get:'getHours', n:' ч.'},
+        {set:'setMinutes', get:'getMinutes', n:' мин.'},
+        {set:'setSeconds', get:'getSeconds', n:' сек.'}
+    ];
+    for (let o of mainArr) {
+        let w=new Date(when);
+        w[o.set](when[o.get]()-1);
+        let i=0;
+        while (cur<=w){
+            i++;
+            cur[o.set](cur[o.get]()+1);
+        }
+        if (i!=0) res.push(i+o.n);
+    }
+
+    return res.join(', ');
+}
+
+getBeforeTime(new Date(2019, 5, 17)); // 1 г., 1 д., 11 ч.
+getBeforeTime(new Date(2018, 5, 15, 13));  // событие завершилось
+getBeforeTime(new Date(2018, 5, 16, 13)); // 1 д.
+
