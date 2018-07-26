fddb21d019cc06f0b66220ba986d15ffe4eeb690
Sudarev Aleksandr <gubernator1900@mail.ru>
2018-07-27 02:27:58 +0300

    Решена задача 16.1

--- 16-module/1-task/index.js
+++ 16-module/1-task/index.js
@@ -3,7 +3,15 @@
  * @param {Generator} gen - генератор
  * @returns {Promise}
  */
-function processGenerator(gen) {
+function processGenerator(func){
+    return new Promise(resolve => {
+
+        function step(result){
+            let f=func.next(result);
+            if (f.done) resolve(f.value); else
+                f.value.then(result => step(result));
+        }
+
+        func.next().value.then(result => step(result));
+    });
 }
-
-
